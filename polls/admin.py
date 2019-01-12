2d11410155373d0e2546bd6fca22e9175b5059f7
Kungreye <kungreye@sina.com>
2019-01-12 21:18:20 +0800

    ChoiceInline from admin.StackedInline

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,21 @@
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # the first element of each tuple is the title of the fieldset.
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}), # `collapse` field will hide.
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
